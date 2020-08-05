770f0e2e75da878363995b0443cc548f2f19e20c
Jaini <761282@cognizant.com>
2020-08-05 20:09:15 +0530

    employee details

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super();
+    this.state = {
+      employees: []
+    }
+  }
+  
+  componentDidMount(){
+    this.getEmpDetails();
+  }
+
+  getEmpDetails=()=>{
+    Axios.get('http://dummy.restapiexample.com/api/v1/employees')
+    .then(response =>{
+      this.setState({employees:response.data.data})
+      console.log(response.data.data)
+    }).catch(error=>{
+      console.log(error)
+    })
+  }
+
+  render() { 
+    return ( 
+      <div>
+        <h2>Employee details</h2>
+        <table border="2">
+          <thead>
+            <tr>
+              <th>EmpId</th>
+              <th>EmpName</th>
+              <th>Age</th>
+              <th>Salary</th>
+            </tr>
+          </thead>
+          <tbody>
+          {this.state.employees.map(function (item, key) {
+
+            return (
+              <tr key={key}>
+                <td>{item.id}</td>
+                <td>{item.employee_name}</td>
+                <td>{item.employee_salary}</td>
+                <td>{item.employee_age}</td>
+              </tr>
+            )
+
+          })
+          }
+        </tbody>
+        </table>
+      </div>
+     );
+  }
 }
-
+ 
 export default App;
